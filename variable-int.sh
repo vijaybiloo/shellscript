5159f2dfea6ac187a633a9bf9c68f285f3c0a691
vijaybiloo <dpk.vijay@gmail.com>
2024-01-13 13:04:12 +0530

    made changes in the file variable-int

--- variable-int.sh
+++ variable-int.sh
@@ -2,5 +2,11 @@
 NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(NUMBER1+NUMBER2)
-echo "Answer is: $SUM"
+SUM=$((NUMBER1+NUMBER2))
+echo "Addition is: $SUM"
+
+DIFF=$((NUMBER1-NUMBER2))
+echo "Difference is: $DIFF
+
+MUL=$((NUMBER1*NUMBER2))
+echo "multiply is: $MUL"
