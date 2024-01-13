0ea55ece7edf563e9e305e5bb3a53ef37f15ffc0
vijaybiloo <dpk.vijay@gmail.com>
2024-01-13 13:42:08 +0530

    created array file

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSONS=("Krishna" "Vishnu" "Rama" "Maa" "Billoo" "Baba" "Khushboo" "Bindiya")
+echo "First person: ${PERSONS[0]}"
+echo "Second person: ${PERSONS[1]}"
+echo "Third person: ${PERSONS[2]}"
+echo "Fourth person: ${PERSONS[3]}"
+echo "Fivth person: ${PERSONS[4]}"
+echo "Sixth person: ${PERSONS[5]}"
+echo "Seventh person: ${PERSONS[6]}"
+echo "Eighth person: ${PERSONS[7]}"
+echo "All person: ${PERSONS[@]}"
