b01f0b04f4549ba7c3e66cd90cf4d0e0945de8b3
vijaybiloo <dpk.vijay@gmail.com>
2024-01-13 11:42:45 +0530

    altered the hello world file

--- hello-world.sh
+++ hello-world.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Hello World"
+
+echo "Hello girls I am learning DevOps in GCP"
+
+PERSON1=Khushboo
+PERSON2=Bindiya
+
+echo "$PERSON1: Hi $PERSON2, Good Morning How is Vijay"
+echo "$PERSON2: Hello $PERSON1, Good Moring Vijay is fine"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
