7a1d1fa7805ec0ae563e6054290ab47945fe7c22
vijaybiloo <dpk.vijay@gmail.com>
2024-01-18 12:47:58 +0530

    created installation file using loops

---
+++ installation-using-loops.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DATA=$(date -%F)
+SCRIPT=$0
+LOG=/home/vijay/shellscripts/$SCRIPT-$DATA.log
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "You should be a root user to run this command"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ....$G SUCCESS $N"
+    fi
+
+}
+
+for i in $@
+do
+
+    yum list installed $i &>>$LOG
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, lets install it"
+        yum install $i -y &>>$LOG
+        VALIDATE $? "$i"
+    else 
+        echo -e "$Y $i is already installed $N"
+    fi
+
+    #yum install $i -y
+done
