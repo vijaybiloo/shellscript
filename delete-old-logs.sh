c996ac5c3aaa0d01444a02f89cc313cc1c3f363e
vijaybiloo <dpk.vijay@gmail.com>
2024-01-19 12:50:05 +0530

    created Deleting old file using while loops

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -6,7 +6,7 @@ DATE=$(date +%F:%H:%M:%S)
 LOGDIR=/home/vijay/shellscript/shellscript.log
 SCRIPT=$0
 LOGFILE=$LOGDIR/$0-$DATE.log
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name ".log -type f -mtime +1")
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name ".log -type f -stime +1")
 
 echo "script started executing at $DATE" &>>LOGFILE
 
