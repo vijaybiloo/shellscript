e7eb59beba4bded9b7a693fc40888c546534a285
vijaybiloo <dpk.vijay@gmail.com>
2024-01-21 18:51:51 +0530

    Created a file for disk usage and sending a mail to gmail

--- disk-usage.sh
+++ disk-usage.sh
@@ -21,4 +21,20 @@ message=""
 #IFS= means internal feild seperator is space.
 while IFS= read line
 do
-    DISK_USAGE | awk '{print $6}' | cut -d % -f1
+    # this command will give you usage in number format for comparision
+    usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
+    # this command will give us partition
+    partition=$(echo $line | awk '{print $1}')
+    #now you need to check whether it is more than threshold or not
+
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ]
+    then
+        message+="HIGH DISK USAGE on $partition: $usage\n"
+    fi
+
+    echo -e "message: $message"
+
+    #echo "$message" | mail -s "High Disk usage" dpk.vijay@gmail.com
+
+    #how to call other shell script from your current script
+    sh mail.sh dpk.vijay@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
